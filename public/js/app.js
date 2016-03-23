79946a15fc333b94077424db7a5d177702991764
Kevin <kevinskyhighhh@hotmail.com>
2016-03-23 19:53:20 -0400

    added nav-bar widget

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
 angular.module('learnNg', []);
+
+angular.module('learnNg')
+      .directive('navBar', function(){
+       return {
+         templateUrl: 'html/templates/nav.html',
+       };
+    });
